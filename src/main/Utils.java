421ac9e13d0705ff2b5d412e8bf19be997c7673c
metroid112 <federico.aguilera112@gmail.com>
2018-11-24 17:36:28 -0300

    Finish even Fibonacci numbers

--- src/main/Utils.java
+++ src/main/Utils.java
@@ -1,8 +1,17 @@
 package main;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Utils {
   
   private static long time;
+  private static List<Long> fibonacciSumArray = new LinkedList<Long>();
+  
+  public Utils() {
+    Utils.fibonacciSumArray.add(0, (long) 0);
+    Utils.fibonacciSumArray.add(1, (long) 1);
+  }
 
   public static void print(String print) {
     System.out.println(print);
@@ -14,5 +23,16 @@ public class Utils {
   
   public static void endTimer() {
     Utils.print("[INFO] Execution time: " + ((System.nanoTime() - Utils.time) / 1000000) + " milliseconds");
+  }
+  
+  public static long fibonacci(int fibonacci) {
+    if (Utils.fibonacciSumArray.size() > fibonacci) {
+      return Utils.fibonacciSumArray.get(fibonacci);
+    } else {
+      for (int i = Utils.fibonacciSumArray.size(); i <= fibonacci; i++) {
+        Utils.fibonacciSumArray.add(i, Utils.fibonacciSumArray.get(i - 1) + Utils.fibonacciSumArray.get(i - 2));
+      }
+      return fibonacciSumArray.get(Utils.fibonacciSumArray.size() - 1);    
+    }
   }
 }
