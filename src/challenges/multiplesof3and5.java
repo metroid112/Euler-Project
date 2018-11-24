a9c0589dc6170278c8e9bdcf367391e69515b8e2
metroid112 <federico.aguilera112@gmail.com>
2018-11-24 16:18:10 -0300

    Start

---
+++ src/challenges/multiplesof3and5.java
@@ -0,0 +1,11 @@
+package challenges;
+
+import main.Utils;
+
+public class multiplesof3and5 {
+
+  public multiplesof3and5() {
+    Utils.print("Starting challenge - Multiples of 3 and 5");
+    
+  }
+}
---
+++ src/main/Utils.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Utils {
+
+  public static void print(String print) {
+    System.out.println(print);
+  }
+}
