39a92d0add865a0c2e2ccc817d5f06e6ee5b9f4e
metroid112 <federico.aguilera112@gmail.com>
2018-11-25 23:53:59 -0300

    IsPrime function

--- src/challenges/LargestPrimeFactor.java
+++ src/challenges/LargestPrimeFactor.java
@@ -1,5 +1,21 @@
 package challenges;
 
+import main.Utils;
+
 public class LargestPrimeFactor {
 
+  public LargestPrimeFactor(long prime) {
+    Utils.startTimer();    
+    Utils.print("[START] Starting challenge - Even Fibonacci numbers");
+
+    long factor = prime / 2;
+    while (!Utils.isPrime(factor) && factor >= 2) {
+      while (prime % factor != 0 && factor >= 2) {
+        factor -= 1;
+      }
+    }
+    
+    Utils.print("[END] The largest prime factor of " + Long.toString(prime) + " is " + Long.toString(factor));
+    Utils.endTimer();
+  }
 }
