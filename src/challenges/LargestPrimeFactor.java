061e35c49f2c9af17bf2ecb88d0a2dcb12dbda90
metroid112 <federico.aguilera112@gmail.com>
2018-11-26 11:38:17 -0300

    Reverse collection

--- src/challenges/LargestPrimeFactor.java
+++ src/challenges/LargestPrimeFactor.java
@@ -1,5 +1,6 @@
 package challenges;
 
+import java.util.Collections;
 import java.util.List;
 
 import main.Utils;
@@ -11,7 +12,11 @@ public class LargestPrimeFactor {
     Utils.print("[START] Starting challenge - Largest Prime factor");
 
     long limit = Math.round(Math.sqrt(prime));
+    long factor = 2;
     List<Boolean> primes = Utils.sieveOfEratosthenes(limit);
+    Collections.reverse(primes);
+    // TODO: For
+    Collections.reverse(primes);
     
     Utils.print("[END] The largest prime factor of " + Long.toString(prime) + " is " + Long.toString(factor));
     Utils.endTimer();
