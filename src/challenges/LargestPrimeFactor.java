0628122b65d4ef93be252d3560ec9989647a64f1
metroid112 <federico.aguilera112@gmail.com>
2018-11-26 12:22:57 -0300

    Regular loop instead of reverse

--- src/challenges/LargestPrimeFactor.java
+++ src/challenges/LargestPrimeFactor.java
@@ -13,10 +13,10 @@ public class LargestPrimeFactor {
 
     long limit = Math.round(Math.sqrt(prime));
     long factor = 2;
-    List<Boolean> primes = Utils.sieveOfEratosthenes(limit);
-    Collections.reverse(primes);
-    // TODO: For
-    Collections.reverse(primes);
+    List<Boolean> sievePrimes = Utils.sieveOfEratosthenes(limit);
+    for (long primes = limit; primes >= 2; primes--) {
+    	
+    }
     
     Utils.print("[END] The largest prime factor of " + Long.toString(prime) + " is " + Long.toString(factor));
     Utils.endTimer();
