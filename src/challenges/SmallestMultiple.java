28ad56b549d6787935488a861ad4288eb612e548
metroid112 <federico.aguilera112@gmail.com>
2018-11-28 13:30:43 -0300

    Start

---
+++ src/challenges/SmallestMultiple.java
@@ -0,0 +1,16 @@
+package challenges;
+
+import main.Utils;
+
+public class SmallestMultiple {
+
+	public SmallestMultiple(int limit) {
+    Utils.startTimer();    
+    Utils.print("[START] Starting challenge - Largest Palindrome product");
+    
+    
+    
+    Utils.print("[END] The largest Palindrome product between 2 numbers of");
+    Utils.endTimer();
+	}
+}
