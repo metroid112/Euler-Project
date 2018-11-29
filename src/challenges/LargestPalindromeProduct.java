cd68f54401517ead354268debaa31a63379c8191
metroid112 <federico.aguilera112@gmail.com>
2018-11-29 14:33:06 -0300

    Prints

--- src/challenges/LargestPalindromeProduct.java
+++ src/challenges/LargestPalindromeProduct.java
@@ -11,32 +11,25 @@ public class LargestPalindromeProduct {
     int largestPalindrome = 1;
     int limit = (int) Math.pow(10, digits) - 1;
     String lowerBoundString = "";
+    String lowBoundString = "1";
     for (int s = 1; s <= digits; s++) {
     	lowerBoundString += Integer.toString(1);
+    	lowBoundString   += Integer.toString(0);
     }
     int lowerBound = Integer.valueOf(lowerBoundString);
-    Utils.debug(lowerBound);
-    Utils.debug(limit);
-//    for (int i = limit; i >= lowerBound; i--) {
-//    	for (int j = limit; j >= lowerBound; j--) {
-//    		Utils.print("[INFO] Checking if " + i + "*" + j + " = " + i*j + " is a Palindrome");
-//    		if (i * j >= largestPalindrome && Utils.isPalindrome(i * j)) {
-//    			Utils.print("[INFO] " + i*j + " is a Palindrome");
-//    			largestPalindrome = i * j;
-//    		}
-//    	}
-//    }
-    int k;
+    int lowBound   = Integer.valueOf(lowBoundString);
+    
+    int i;
     boolean found = false;
-    for (k = limit * limit; !found && k >= lowerBound; k--) {
-    	for (int l = lowerBound * 9; !found && l >= 1; l--) {
-    		if (k % l == 0 && k / l < 1000 && Utils.isPalindrome(k)) {
+    for (i = limit * limit; !found && i >= lowerBound; i--) {
+    	for (int j = lowerBound * 9; !found && j >= 1; j--) {
+    		if (i % j == 0 && i / j < lowBound && Utils.isPalindrome(i)) {
     			found = true;
     		}
     	}
     }
-    // TODO: Optimize
-    Utils.print("found it " + (k + 1));
+    // TODO: Prints
+    Utils.print("found it " + (i + 1));
     
     Utils.print("[END] The largest Palindrome product between 2 numbers of " + digits + " digits is " + largestPalindrome);
     Utils.endTimer();
