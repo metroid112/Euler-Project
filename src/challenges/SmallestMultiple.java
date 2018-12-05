31041bfff5759437e58075cc61e57aabf11ab623
metroid112 <federico.aguilera112@gmail.com>
2018-12-05 14:34:31 -0300

    Fix error and continue Product

--- src/challenges/SmallestMultiple.java
+++ src/challenges/SmallestMultiple.java
@@ -2,19 +2,20 @@ package challenges;
 
 import java.util.Arrays;
 
-import main.Utils;
+import static main.Utils.*;
+import static main.Utils.PRINT.*;
 
 public class SmallestMultiple {
 
 	public SmallestMultiple(int limit) {
-    Utils.startTimer();    
-    Utils.print("[START] Starting challenge - Smallest Multiple");
+    startTimer();    
+    print(START, "Starting challenge - Smallest Multiple");
     
     int[] factors = new int[limit];
     Arrays.setAll(factors, a -> a + 1);
-  	int lcm = Utils.lcm(factors);
+  	int lcm = lcm(factors);
     
-    Utils.print("[END] The smallest multiple of numbers 1 to " + limit + " is " + lcm);
-    Utils.endTimer();
+    print(END, "The smallest multiple of numbers 1 to " + limit + " is " + lcm);
+    endTimer();
 	}
 }
